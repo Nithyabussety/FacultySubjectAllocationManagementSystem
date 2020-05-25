9bed81197ca3a816cceac30e5893fa5f1ac34d32
Nithya B <nithyabussety16@gmail.com>
2020-05-25 16:13:06 +0530

    added admin files

---
+++ FacultySubjectAllocationManagementSystem/trms/admin/includes/sidebar.php
@@ -0,0 +1,60 @@
+<aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+<
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button> 
+                <a class="navbar-brand" href="dashboard.php">TRMS ADMIN | </a>
+                
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="dashboard.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+<li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Subjects</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="add-subjects.php">Add Subjects</a></li>
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="manage-subjects.php">Manage Subjects</a></li>
+                        </ul>
+                    </li>
+<li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Teacher</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="add-teacher.php">Add Teacher</a></li>
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="manage-teacher.php">Manage Teacher</a></li>
+                        </ul>
+                    </li>
+<li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>HOD</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="add-hod.php">Add HOD</a></li>
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="manage-hod.php">Manage HOD</a></li>
+                        </ul>
+                    </li>
+<li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Department</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="add-dept.php">Add Department </a></li>
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="manage-dept.php">Manage Department</a></li>
+                        </ul>
+                    </li>
+
+<li class="active">
+                        <a href="search.php"> <i class="menu-icon fa fa-search"></i>Search </a>
+                    </li>
+  <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Reports</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-file-o"></i><a href="bwdates-report-ds.php">Between Dates Report</a></li>
+                           
+                        </ul>
+                    </li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside>
