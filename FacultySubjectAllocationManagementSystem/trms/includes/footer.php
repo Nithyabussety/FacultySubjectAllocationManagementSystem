15bd7800513018f1061445ed022355a20d7806ff
Nithya B <nithyabussety16@gmail.com>
2020-05-25 16:07:09 +0530

    added others

---
+++ FacultySubjectAllocationManagementSystem/trms/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer-area section_gap">
+        <div class="container" align="center">
+
+    <h2 align="center" style="color:#fff"> Faculty Subject Allocation Management System</h2>
+   
+        </div>
+    </footer>
---
+++ FacultySubjectAllocationManagementSystem/trms/includes/header.php
@@ -0,0 +1,63 @@
+<!--================ Start Header Menu Area =================-->
+    <header class="header_area">
+        
+        <div class="header-menu">
+              <div class="col-sm-7">
+                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                    <div class="header-left">
+                       
+                        <div class="form-inline">
+                            
+                        </div>
+                    
+                        <div class="form-inline">
+                            
+                        </div>
+
+                      
+                    </div>
+                </div>
+            
+
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                   <h3><a href="index.php">Faculty Subject Allocation Management System</a></h3>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+      
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="container-fluid">
+                    <div class="collapse navbar-collapse offset" style="margin: 5px" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto">
+                            <li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li>
+                           </ul>
+                    
+                   <div class="user-area dropdown float-right">
+                     <button class="btn btn-primary dropdown-toggle" type="button" style="margin: 5px" data-toggle="dropdown">Login As
+                    <span class="caret"></span></button>
+                     <ul class="dropdown-menu">
+                       <li><a href="admin/index.php">Admin</a></li>
+                      <li><a href="faculty/index.php">Faculty</a></li>
+                       <li><a href="hod/index.php">HOD</a></li>
+                         </ul>
+                      </div>
+                      </div> 
+                </div>
+            
+        
+            
+            
+    </nav>
+ </div>
+</header>
+    <!--================ End Header Menu Area =================-->
+   
+
+
+    
